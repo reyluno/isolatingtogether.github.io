218f79af632fe32fa083b65b410966eb9d7770fc
pbt-santos <pbt.santos@outlook.com>
2020-06-09 12:55:04 +0100

    inital part of automatic submitter and associated environment requirements. needs auth

--- utils/dbUpdate.py
+++ utils/dbUpdate.py
@@ -1,22 +1,53 @@
+''' 
+This file is used to ge through and upload metadata to cloud
+storage for artist submissions
+'''
+
 #imports
 import os
-#dependencies
+from google.cloud import firestore
+import google.auth
 
 # can potentially execute the cloud utils rsync here if we want all in one
 
 # authorization and connect method
 def cloudConnect(debug=False):
+    # let's connect and add a new document
+    # get credentials
+    # use credentials to login to client
+    # return the client instance
+    db = firestore.Client()
+    
     if debug:
         print("Connected to cloud")
 
-def uploadData(debug=False):
+    return db
+
+def uploadData(db, directory, debug=False):
+    # when we get to the final version, this will use directory to be specific
+
+    doc_ref = db.collection(u'artists').document(u'johndoe')
+    
+    # fill in the document's metadata
+    # name, email and first contact date
+    doc_ref.set({
+        u'name': u'John Doe',
+        u'email': u'john.doe@gmail.com',
+        u'fc_date': u'01/01/2020'
+    })
+    
+    # check if it worked
+    users_ref = db.collection(u'artists')
+
+    for doc in users_ref.stream():
+        print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
+    # create a subcollection for the image metadata
+
+    # go through each of the images uploaded and fill in their metadata
+
     if debug:
         print("Uploaded artist and image metadata")
-        # fill in the document's metadata
-
-        # create a subcollection for the image metadata
-
-        # go through each of the images uploaded and fill in their metadata
 
 # function to go through each folder and upload the artist and image metadata
 # use last upload time to determine whether to change metadata?
@@ -34,10 +65,10 @@ def uploadArtistMetadata(artist="", debug=False):
                 # here we know that we found artist
                 if debug:
                     print("Found {} for {}".format(directory, artist))
-                uploadData(debug)
+                uploadData(directory, debug)
                 return # finished at this point
 
-            uploadData(debug)
+            uploadData(directory, debug)
 # main 
 def main(debug=False):
     # get client and authorize the credentials locally
