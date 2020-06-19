de18845dc65ce14ad53c40cd7c84375e36890d75
Trey Gilliland <jlg2266@columbia.edu>
2020-06-19 13:19:59 -0400

    Reordered images

--- public/js/create.js
+++ public/js/create.js
@@ -3,7 +3,9 @@ var currLen = 0;
 var currRow;
 var container = document.getElementById("grid");
 var baseURL = "https://github.com/reyluno/isolatingtogether.github.io/raw/master/img/create/";
-var imgData = ['AlbertGwo2.jpg', 'RommelNunez3.jpg', 'MadeleineMueller.jpeg', 'LukeBolster3.jpg', 'KyleeDanks1.png', 'RubaNadar1.jpeg', 'AdeBalogun2.jpg', 'AlbertGwo12.jpg', 'AlbertGwo11.jpg', 'ElianaRodgers3.jpg', 'RobertHunter.jpg', 'ElianaRodgers2.jpg', 'RubaNadar2.jpeg', 'CarlosOchoa1.jpg', 'RommelNunez1.jpg', 'AlbertGwo10.jpg', 'AlbertGwo4.jpg', 'GrantPace2.png', 'GrantPace1.jpg', 'LukeBolster4.jpg', 'AlbertGwo1.jpg', 'ElianaRodgers1.jpg', 'AlbertGwo5.jpg', 'AlbertGwo6.JPG', 'AdeBalogun3.jpg', 'RubaNadar4.jpeg', 'AlbertGwo9.jpg', 'AlbertGwo8.jpg', 'LukeBolster2.jpg', 'AlbertGwo7.jpg', 'CameronLee.jpg', 'RubaNadar3.jpeg', 'RommelNunez2.jpg', 'AdeBalogun1.jpg', 'FergusCampbell.gif', 'AshleyJiao.png', 'AlbertGwo3.jpg', 'LukeBolster1.jpeg'];
+var imgData = ['AlbertGwo2.jpg', 'RommelNunez3.jpg', 'AlbertGwo8.jpg', 'AlbertGwo5.jpg',
+    'KyleeDanks1.png', 'RubaNadar1.jpeg', 'AdeBalogun2.jpg', 'AlbertGwo12.jpg', 'AlbertGwo11.jpg', 'ElianaRodgers3.jpg', 'RobertHunter.jpg', 'ElianaRodgers2.jpg', 'RubaNadar2.jpeg', 'CarlosOchoa1.jpg', 'RommelNunez1.jpg', 'AlbertGwo10.jpg', 'AlbertGwo4.jpg', 'GrantPace2.png', 'GrantPace1.jpg', 'LukeBolster4.jpg', 'AlbertGwo1.jpg', 'ElianaRodgers1.jpg', 'LukeBolster3.jpg', 'AlbertGwo6.JPG', 'AdeBalogun3.jpg', 'RubaNadar4.jpeg', 'AlbertGwo9.jpg', 'MadeleineMueller.jpeg', 'LukeBolster2.jpg', 'AlbertGwo7.jpg', 'CameronLee.jpg', 'RubaNadar3.jpeg', 'RommelNunez2.jpg', 'AdeBalogun1.jpg', 'FergusCampbell.gif', 'AshleyJiao.png', 'AlbertGwo3.jpg', 'LukeBolster1.jpeg'
+];
 
 imgData.forEach(addImage);
 
