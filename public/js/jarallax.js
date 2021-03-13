72648a7c81bd3fd9f09b195a92fb8427efb91c68
Nick <nickaldanarod@gmail.com>
2021-03-13 13:36:38 -0500

    js update

---
+++ public/js/jarallax.js
@@ -0,0 +1,13 @@
+// object-fit polyfill run
+objectFitImages();
+
+/* init Jarallax */
+jarallax(document.querySelectorAll('.jarallax'));
+
+jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+    keepImg: true,
+});
+
+$('.jarallax').jarallax({
+  speed: 0.2
+});
